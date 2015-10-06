d0bde12372eb640708d03dfc9cedd5125903c7e9
Alison Ostendorf <aostendorf@gmail.com>
2015-10-05 21:20:21 -0400

    amazon s3 coordination

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,15 @@ Rails.application.configure do
 
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # config/environments/production.rb
+  # required to test the aws-sdk in the development environment
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
