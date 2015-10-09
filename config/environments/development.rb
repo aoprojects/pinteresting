7200025b690f9a4a641f4a0b30da97bd8770c8a3
Alison Ostendorf <aostendorf@gmail.com>
2015-10-09 15:41:18 -0400

    just checking

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,13 +43,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # config/environments/production.rb
-  # required to test the aws-sdk in the development environment
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+  # required to test the aws-sdk in the development environment, to use with Paperclip
+  #config.paperclip_defaults = {
+  #  :storage => :s3,
+  #  :s3_credentials => {
+  #    :bucket => ENV['S3_BUCKET_NAME'],
+  #    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #}
+#}
 end
